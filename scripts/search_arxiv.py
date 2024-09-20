9502c2c430d4a2ed4ac74ecc219e72c788408e77
hustzxd <2227820226@qq.com>
2024-09-20 12:08:33 +0800

    weekly paper

---
+++ scripts/search_arxiv.py
@@ -0,0 +1,42 @@
+import datetime as DT
+
+import arxiv
+
+today = DT.date.today()
+week_ago = today - DT.timedelta(days=7)
+
+key_words = ["sparse", "pruning", "sparsity", "quantize", "quantization", "low-bit", "LLM"]
+
+query = ""
+for i, k in enumerate(key_words):
+    if i == 0:
+        query = f"abs:{k}"
+    query += f" AND abs:{k}"
+# Construct the default API client.
+client = arxiv.Client()
+
+# Search for the 10 most recent articles matching the keyword "quantum."
+search = arxiv.Search(
+    query="abs:sparse AND abs:quantization AND abs:sparsity AND abs:quantize",
+    max_results=100,
+    sort_by=arxiv.SortCriterion.SubmittedDate,
+)
+
+markdown_content = ""
+# `results` is a generator; you can iterate over its elements one by one...
+for paper in client.results(search):
+    date = paper.published.date()
+    if date > week_ago:
+        title = paper.title
+        print(title)
+        authors = [author.name for author in paper.authors]
+        authors = ",".join(authors)
+        url = paper.entry_id
+        summary = paper.summary
+        markdown_content += f"# {title}\n\n"
+        markdown_content += f">Authors: {authors}\n\n"
+        markdown_content += f"> {url}\n\n"
+        markdown_content += f"{summary}\n\n\n"
+
+with open(f"/Users/xiandong/projects/EfficientPaper/weekly_paper/{today}.md", "w") as wf:
+    wf.write(markdown_content)
---
+++ weekly_paper.sh
@@ -0,0 +1,1 @@
+python /Users/xiandong/projects/EfficientPaper/scripts/search_arxiv.py
